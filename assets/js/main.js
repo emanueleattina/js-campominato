88386535b1bacc66452cf4c92ad0a2a18a971696
emanuele <emattina97@gmail.com>
2021-03-09 15:08:52 +0100

    Carico scelta difficoltà

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,46 @@
-// il computer sceglie 16 numeri casuali da 1 a 100 non duplicati
+// FATTO il computer sceglie 16 numeri casuali da 1 a 100 non duplicati
 // TODO chiedere all'utente 100 - 16 volte di inserire un numero da 1 a 100 non duplicati
 // TODO se il numero è presente nella lista dei random la partita finisce
 // TODO se l'utente inserisce tutti e 100 - 16 numeri la partita finisce
 // TODO stampare alla fine il punteggio
+// FATTO bonus: livello difficoltà
+
+// inserire livello di difficoltà
+do
+{
+  var diff = parseInt(prompt("Il gioco propone difficoltà da 1 a 3, scegline una:"));
+
+  if (diff > 3 || diff < 1 || (isNaN(diff)))
+  {
+    alert("Inserisci la difficoltà corretta");
+  }
+} while(diff > 3 || diff < 1 || (isNaN(diff)));
+
+switch (diff)
+{
+  case 1:
+    max = 100;
+    break;
+
+  case 2:
+    max = 80;
+    break;
+
+  case 3:
+    max = 50;
+    break;
+
+  default:
+    break;
+}
+console.log(max);
+
 
 var arrayBomba = [];
 
 while (arrayBomba.length < 16)
 {
-  var bomba = numeroRandom(1, 100)
+  var bomba = numeroRandom(1, max)
   if (!arrayBomba.includes(bomba))
   {
     arrayBomba.push(bomba);
@@ -22,6 +54,5 @@ console.log(arrayBomba);
 
 function numeroRandom(min, max)
 {
-  var numeroBomba = Math.floor(Math.random() * max) + min;
-  return numeroBomba;
+  return Math.floor(Math.random() * max) + min;
 }
